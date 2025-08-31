66d5e2f2fc003c5e3e96465cbf940aa60af89b17
Siobhan Downes <downes.siobhan@gmail.com>
2025-08-31 11:27:35 +0100

    Final static file config for render deployment

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+# Define BASE_DIR manually
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Wrap with WhiteNoise to serve static files
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
